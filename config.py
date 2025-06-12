d6e9f09ff5636f34f5529e259b300eb8e7751c8f
Xiaoying2005 <3519871986@qq.com>
2025-06-12 15:30:42 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,2 @@
+# Insert your OpenAI API key below
+OPENAI_API_KEY = "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"
